219e925b0af82eb79362cf7c7cdbb0b2bef08d59
Ьustafin_Maksin <mustafinmp@yandex.ru>
2021-03-13 19:51:40 +0500

    исправление багов с дизайном

--- venv/main.py
+++ venv/main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
 from data.users import User
 from data.news import News
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+def main_page():
+    return render_template('main.html', blogs='/blogs')
 
 
 def main():
