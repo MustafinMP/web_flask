df9c3c06b6fe4ee79e35756351963deed67027e6
Ьustafin_Maksin <mustafinmp@yandex.ru>
2021-03-13 21:39:09 +0500

    выделение из главного шаблона базовые элементы в отдельный файл для последующего наследования

--- venv/main.py
+++ venv/main.py
@@ -9,7 +9,14 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route('/')
 def main_page():
-    return render_template('main.html', blogs='/blogs')
+    return render_template('main.html', blogs='/blogs',
+                           gallery='/gallery',
+                           breed='/breed')
+
+
+@app.route('/blogs')
+def blogs_page():
+    return render_template('blogs_page.html')
 
 
 def main():
